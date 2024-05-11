96b2646d421b9459d66db7035b2c4bb070728a58
HelioFernandes404 <heliojobs25@gmail.com>
2024-05-11 12:58:04 -0300

    init

---
+++ src/main/java/org/helio/fullrestapispring/FullrestapispringApplication.java
@@ -0,0 +1,13 @@
+package org.helio.fullrestapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullrestapispringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullrestapispringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/helio/fullrestapispring/FullrestapispringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helio.fullrestapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullrestapispringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
