12ccb4c855f1661a07c63ea694ad1cc5fbeb212b
HelioFernandes404 <heliojobs25@gmail.com>
2024-05-13 14:53:50 -0300

    Implementando as interfaces UserDetails

--- src/main/java/org/helio/fullrestapispring/security/userdetails/UserDetailsImpl.java
+++ src/main/java/org/helio/fullrestapispring/security/userdetails/UserDetailsImpl.java
@@ -1,4 +1,66 @@
 package org.helio.fullrestapispring.security.userdetails;
 
-public class UserDetailsImpl {
+import lombok.Getter;
+import org.helio.fullrestapispring.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private User user; // Classe de usuário que criamos anteriormente
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        /*
+         Este método converte a lista de papéis (roles) associados ao usuário
+         em uma coleção de GrantedAuthorities, que é a forma que o Spring Security
+         usa para representar papéis. Isso é feito mapeando cada papel para um
+         novo SimpleGrantedAuthority, que é uma implementação simples de
+         GrantedAuthority
+        */
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    } // Retorna a credencial do usuário que criamos anteriormente
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    } // Retorna o nome de usuário do usuário que criamos anteriormente
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
--- src/main/java/org/helio/fullrestapispring/security/userdetails/UserDetailsServiceImpl.java
+++ src/main/java/org/helio/fullrestapispring/security/userdetails/UserDetailsServiceImpl.java
@@ -1,4 +1,23 @@
 package org.helio.fullrestapispring.security.userdetails;
 
-public class UserDetailsServiceImpl {
+import org.helio.fullrestapispring.entities.User;
+import org.helio.fullrestapispring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+        return new UserDetailsImpl(user);
+    }
+
 }
