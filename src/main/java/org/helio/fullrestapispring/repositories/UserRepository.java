e22bd84c0a46882c95789008ef117fb26d498cc2
HelioFernandes404 <heliojobs25@gmail.com>
2024-05-13 14:44:02 -0300

    repositorio de usuario

---
+++ src/main/java/org/helio/fullrestapispring/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package org.helio.fullrestapispring.repositories;
+
+import org.helio.fullrestapispring.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
